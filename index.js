f386ba88e13c385337a8a36e37bf8777f7801237
Valikhanov19 <abdukakhkhorvalikhanoff@gmail.com>
2022-12-02 11:59:45 +0300

    homework

---
+++ index.js
@@ -0,0 +1,46 @@
+// let num = prompt ("enter number");
+
+// if (num >= 0 && num <= 9){
+//     console.log("single digit numbers");
+// }else if (num >= 10 && num <= 99){
+//     console.log("two digit numbers");
+// }else if (num >= 100 && num <= 999){
+//     console.log("three digit numbers ");
+// }else if (num >= 1000 && num <= 9999){
+//     console.log("four digit numbers");
+// }else{
+//     console.log("Please! enter four digit numbers!");
+// }
+
+
+
+//  let a = prompt('Enter number')
+
+
+//  if(isNaN(a)){
+//      console.log('It is not a number((');
+//  }else{
+//      console.log('It is number))');
+//  }
+
+
+
+
+// let km = prompt ("Enter Kilometr")
+
+// let human = 3.6;
+// let bicycle = 20.1;
+// let vehicle = 70;
+// let airplane = 800;
+
+// console.log(`human ${ Math.floor((km / human))} hour ${Math.floor((km % human) / human * 60)} minute`);
+// console.log(`bicycle ${ Math.floor((km / bicycle))} hour ${Math.floor((km % bicycle) / bicycle * 60)} minute`);
+// console.log(`vehicle ${ Math.floor((km / vehicle))} hour ${Math.floor((km % vehicle) / vehicle * 60)} minute`);
+// console.log(`airplane ${ Math.floor((km / airplane))} hour ${Math.floor((km % airplane) / airplane * 60)} minute`);
+
+
+// let a = prompt("enter a")
+// let b = prompt("enter b")
+// let c = prompt("enter c")
+
+// console.log(Math.max(a,b,c));
